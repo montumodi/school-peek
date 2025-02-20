cdb3ad54357be3fac79dd540e72646288673ab2f
Ashish Modi <montumodi@gmail.com>
2025-02-20 11:20:27 +0000

    added entry file

--- src/utils/embedding_utils.py
+++ src/utils/embedding_utils.py
@@ -1,11 +1,17 @@
 import torch
 from transformers import AutoModel, AutoTokenizer
+from functools import lru_cache
 
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
+
+@lru_cache(maxsize=1)
+def get_model_and_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    return tokenizer, model
 
 def get_transformer_embedding(text):
+    tokenizer, model = get_model_and_tokenizer()
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
     with torch.no_grad():
         outputs = model(**inputs)
